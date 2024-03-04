a87b6367d01a26886bc2d3bff78884a679917e1e
JuliaSukach <juliasukach17@gmail.com>
2024-03-04 11:21:53 +0100

    small updated

--- client/src/feed/Feed.js
+++ client/src/feed/Feed.js
@@ -1,11 +1,11 @@
-import React, { useEffect, useState } from "react";
-import "../css/feed/Feed.css";
-import Post from "./Post";
-import TweetBox from "./TweetBox";
-import { collection, getDocs, query, where, } from "firebase/firestore";
-import db from "../firebase/firebase";
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { auth } from '../firebase/firebase'; // Replace with your actual Firebase config import
+import React, { useEffect, useState } from 'react'
+import '../css/feed/Feed.css'
+import Post from './Post'
+import TweetBox from './TweetBox'
+import { collection, getDocs, query, where, } from 'firebase/firestore'
+import db from '../firebase/firebase'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { auth } from '../firebase/firebase' // Replace with your actual Firebase config import
 
 function Feed() {
     const [user] = useAuthState(auth)
@@ -15,25 +15,20 @@ function Feed() {
             const querySnapshot = await getDocs(
                 query(collection(db, 'posts'), where('displayName', '==', user.displayName))
             )
-            const newData = querySnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+            const newData = querySnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
             setPosts(newData)
         } catch (error) {
-            console.error('Error fetching posts:', error);
+            console.error('Error fetching posts:', error)
         }
-        await getDocs(collection(db, 'posts'))
-            .then((querySnapshot) => {
-                const newData = querySnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
-                setPosts(newData)
-            })
     }
 
     useEffect(() => {
         fetchPost()
-    }, []);
+    }, [])
 
     return (
-        <div className="feed">
-            <div className="feed__header">
+        <div className='feed'>
+            <div className='feed__header'>
                 <h2>Home</h2>
             </div>
             <TweetBox onTweetAdded={fetchPost}/>
@@ -50,7 +45,7 @@ function Feed() {
                 />
             ))}
         </div>
-    );
+    )
 }
 
-export default Feed;
+export default Feed
